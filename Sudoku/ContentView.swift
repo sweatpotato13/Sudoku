ae7e17598ef85febbc6cfe938a7df6cdb6495ce0
JangChorong <sweatpotato13@gmail.com>
2020-05-24 20:19:52 +0900

    Init commit

---
+++ Sudoku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudoku
+//
+//  Created by mac on 2020/05/24.
+//  Copyright © 2020 CuteWisp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
